0744ebe414ed34647478b63e693fa835b074fa14
gaozhengxin <zhengxingao@live.cn>
2022-10-31 18:22:50 +0800

    rename MerkleProof->verifyMerkle proof

--- verifyMerkleProof/workspace-depth10-gm17/run.sh
+++ verifyMerkleProof/workspace-depth10-gm17/run.sh
@@ -1,6 +1,6 @@
 zokrates compile -i ../src/depth10.zok
 zokrates setup -s gm17
 zokrates compute-witness --stdin < data.input
-zokrates generate-proof -s gm17
+zokrates generate-proof -s gm17 # 1m10s
 zokrates export-verifier
 zokrates verify
